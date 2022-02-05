6854c47cd262d252410b1e15a3a533d27f96404c
sid-shakthivel <sid.shakthivel@gmail.com>
2022-02-05 22:52:37 +0000

    Fixed Sprite Bug

--- src/gpu.rs
+++ src/gpu.rs
@@ -54,23 +54,27 @@ impl GPU {
                 // V-Blank Interrupt
                 self.mmu.borrow_mut().request_interrupt(0);
             }
+
+            if current_scanline < 144 {
+                self.draw_scanline();
+            }
         }
     }
 
     fn set_lcd_status(&mut self) {
         if self.is_lcd_enabled() == 0 {
-            self.scanline_counter = 0;
+            self.scanline_counter = 456;
             self.mmu.borrow_mut().io_ram[0xFF44 - 0xFF00] = 0;
-            let status: u8 = self.mmu.borrow_mut().rb(0xFF41) | 0x1;
+            let status: u8 = self.mmu.borrow().rb(0xFF41) | 0x1;
             self.mmu.borrow_mut().wb(0xFF41, status);
             return;
         }
 
-        let current_mode = self.mmu.borrow_mut().rb(0xFF41) & 0x3;
-        let current_scanline: u8 = self.mmu.borrow_mut().rb(0xFF44);
+        let current_mode = self.mmu.borrow().rb(0xFF41) & 0x3;
+        let current_scanline: u8 = self.mmu.borrow().rb(0xFF44);
         let mut new_mode = 0;
         let mut check_bit = 0;
-        let status: u8 = self.mmu.borrow_mut().rb(0xFF41);
+        let status: u8 = self.mmu.borrow().rb(0xFF41);
 
         if current_scanline > 144 {
             // V-Blank
@@ -91,7 +95,6 @@ impl GPU {
             new_mode = 0;
             self.mmu.borrow_mut().wb(0xFF41, status & 252);
             check_bit = status & (1 << 5);
-            self.draw_scanline();
         }
 
         if new_mode != current_mode && check_bit > 0 {
@@ -99,31 +102,30 @@ impl GPU {
         }
 
         // Coincidence Flag
-        if current_scanline == self.mmu.borrow_mut().rb(0xFF45) {
+        if current_scanline == self.mmu.borrow().rb(0xFF45) {
             self.mmu.borrow_mut().wb(0xFF41, status | (1 << 2));
         } else {
             self.mmu.borrow_mut().wb(0xFF41, status & !(1 << 2));
         }
 
-        if self.mmu.borrow_mut().rb(0xFF41) & (1 << 2) == 1
-            && self.mmu.borrow_mut().rb(0xFF41) & (1 << 6) == 1
+        if self.mmu.borrow().rb(0xFF41) & (1 << 2) == 1
+            && self.mmu.borrow().rb(0xFF41) & (1 << 6) == 1
         {
             self.mmu.borrow_mut().request_interrupt(1);
         }
     }
-
     fn is_lcd_enabled(&self) -> u8 {
         self.mmu.borrow_mut().rb(0xFF40) & (1 << 7)
     }
 
     fn draw_scanline(&mut self) {
         let lcd_control = self.mmu.borrow_mut().rb(0xFF40);
-        if lcd_control & (1 << 1) > 0 {
-            self.render_sprites();
-        }
 
         if lcd_control & (1 << 0) == 1 {
             self.render_tiles();
+        }
+        if lcd_control & (1 << 1) > 0 {
+            self.render_sprites();
         }
     }
 
@@ -140,15 +142,15 @@ impl GPU {
         // Lookup tile data in tile data region
         // Get 2 Bytes & Identify Colour
 
-        let scroll_y: u8 = self.mmu.borrow_mut().rb(0xFF42); // Y position of BACKGROUND to draw view
-        let scroll_x: u8 = self.mmu.borrow_mut().rb(0xFF43); // X position of BACKGROUND to draw view
+        let scroll_y: u8 = self.mmu.borrow().rb(0xFF42); // Y position of BACKGROUND to draw view
+        let scroll_x: u8 = self.mmu.borrow().rb(0xFF43); // X position of BACKGROUND to draw view
 
-        let window_y: u8 = self.mmu.borrow_mut().rb(0xFF4A); // Y position of VIEW to draw window
-        let window_x: u8 = self.mmu.borrow_mut().rb(0xFF4B); // X position of VIEW to draw window
+        let window_y: u8 = self.mmu.borrow().rb(0xFF4A); // Y position of VIEW to draw window
+        let window_x: u8 = self.mmu.borrow().rb(0xFF4B); // X position of VIEW to draw window
 
-        let lcd_control: u8 = self.mmu.borrow_mut().rb(0xFF40);
-        let current_scanline: u8 = self.mmu.borrow_mut().rb(0xFF44);
-        let colour_palette: u8 = self.mmu.borrow_mut().rb(0xFF47);
+        let lcd_control: u8 = self.mmu.borrow().rb(0xFF40);
+        let current_scanline: u8 = self.mmu.borrow().rb(0xFF44);
+        let colour_palette: u8 = self.mmu.borrow().rb(0xFF47);
 
         let tile_identity_address: u16; // Location in which tile identification numbers are stored
         let mut tile_data_address: u16; // Location in which tile data (pixel information) is stored
@@ -224,6 +226,8 @@ impl GPU {
                 tile_data_address = 0x8000 + (unsigned_tile_identifier * 16);
             }
 
+            // tile_data_address = 0x8580;
+
             let mut line: u16 = (y_pos % 8) as u16;
             line *= 2;
 
@@ -250,6 +254,10 @@ impl GPU {
                 res = res << 8 | (rgb.1 as u32);
                 res = res << 8 | (rgb.2 as u32);
 
+                if tile_data_address == 0x8340 {
+                    // println!("{}", res);
+                }
+
                 let test: usize = j as usize + base as usize;
                 if current_scanline <= 143 && test <= 159 {
                     let index: usize =
@@ -260,7 +268,7 @@ impl GPU {
         }
     }
 
-    fn render_sprites(&mut self) {
+    pub fn render_sprites(&mut self) {
         let lcd_control: u8 = self.mmu.borrow_mut().rb(0xFF40);
         let current_scanline: u8 = self.mmu.borrow_mut().rb(0xFF44);
         let is_8x8: bool = lcd_control & (1 << 2) == 0;
@@ -272,12 +280,12 @@ impl GPU {
             // edit scanline + actual line + bit (1 of 8) + set to a 32 bit number
 
             let offset: u16 = i * 40;
-            let x_pos: u8 = self
+            let y_pos: u8 = self
                 .mmu
                 .borrow()
                 .rb(offset.wrapping_add(0xFE00))
                 .wrapping_sub(16);
-            let y_pos: u8 = self
+            let x_pos: u8 = self
                 .mmu
                 .borrow()
                 .rb(offset.wrapping_add(0xFE00).wrapping_add(1))
@@ -296,20 +304,19 @@ impl GPU {
                 current_colour_palette = self.mmu.borrow_mut().rb(0xFF48);
             }
 
-            if tile_identifier == 0x58 {
-                // println!("{} {} {}", i, x_pos, y_pos);
-            }
-
-            let y_offset = if is_8x8 { 8 } else { 16 };
+            let mut y_offset = if is_8x8 { 8 } else { 16 };
+            y_offset = 8;
 
             if current_scanline >= y_pos && current_scanline <= (y_pos + y_offset) {
-                let tile_data_address: u16 = 0x8000 + (tile_identifier * 16);
                 let mut line: u16 = (current_scanline as u16) - (y_pos as u16);
                 line *= 2;
-                let data1 = self.mmu.borrow_mut().rb(tile_data_address + line);
-                let data2 = self.mmu.borrow_mut().rb(tile_data_address + line + 1);
+                let tile_data_address: u16 = 0x8000 + (tile_identifier * 16) + line;
+
+                let data1 = self.mmu.borrow_mut().rb(tile_data_address);
+                let data2 = self.mmu.borrow_mut().rb(tile_data_address + 1);
                 for mut j in (0..8).rev() {
                     let data_colour: u8 = self.get_bit(data2, j) << 1 | self.get_bit(data1, j);
+
                     let rgb = match data_colour {
                         0b00 => self.get_colour(current_colour_palette, 0),
                         0b01 => self.get_colour(current_colour_palette, 2),
@@ -319,16 +326,19 @@ impl GPU {
                             panic!("Wrong Combination");
                         }
                     };
+
                     j -= 7;
                     j *= -1;
+
                     let mut res: u32 = 0;
                     res = res << 8 | (rgb.0 as u32);
                     res = res << 8 | (rgb.1 as u32);
                     res = res << 8 | (rgb.2 as u32);
 
-                    let temp = x_pos.wrapping_add(j as u8);
-                    if current_scanline <= 143 && (temp) <= 159 {
-                        let index: usize = ((current_scanline) as usize * 160) + (temp as usize);
+                    let test: usize =
+                        ((current_scanline) as usize * 160) + j as usize + x_pos as usize;
+                    let index: usize = test;
+                    if current_scanline < 144 && (j as usize + x_pos as usize) < 160 {
                         self.screen_data[index] = res;
                     }
                 }
